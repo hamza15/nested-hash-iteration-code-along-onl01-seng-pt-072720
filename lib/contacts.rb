1f1d8f4c6722de9c3d2fb61183465c475faecb8d
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-04 04:20:30 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |name, data|
-    if name == "Freddy"
+    if name == "Freddy Mercury"
       data.each do |key, value|
         puts "#{key}: #{value}"
       end
